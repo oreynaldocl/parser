9cdc244939197b3acc243742f02d6094b331108a
Oscar Callisaya <oreynaldo.cl@gmail.com>
2025-06-10 23:26:57 -0400

    Created initial files

---
+++ src/Parser.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Parser.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //Worker worker = new Worker();
+        }
+    }
+}
